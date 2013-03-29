8507a4d661556f9a116c7bf757d9b89ee0897bfd
Jeffrey Tong <trigun0x2@gmail.com>
2013-03-29 18:35:33 -0400

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jquery-rails', '2.0.2'
 group :test do
   gem 'capybara', '1.1.2'
   gem 'rb-fsevent', '0.9.1', :require => false
+  gem 'factory_girl_rails', '4.1.0'
 end
 
 group :production do
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
-  before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
