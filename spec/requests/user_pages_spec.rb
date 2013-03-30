1cfc03f4818287e92871abaa6001814802872f2a
Jeffrey Tong <trigun0x2@gmail.com>
2013-03-30 17:49:19 -0400

    finished signin

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,17 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+
+    	describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should have_content('Password') }
+        it { should have_content('Name') }
+        it { should have_content('Email') }
+      end
+
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
@@ -43,6 +54,15 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
