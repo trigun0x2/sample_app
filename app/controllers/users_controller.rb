5650f98a1e703f73da2415732878dab25255daa2
Jeffrey Tong <trigun0x2@gmail.com>
2013-04-01 16:21:58 -0400

    listing 9.23

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_filter :signed_in_user, only: [:edit, :update]
+	before_filter :signed_in_user, only: [:index, :edit, :update]
 	before_filter :correct_user,   only: [:edit, :update]
 
 	def show
@@ -22,6 +22,10 @@ class UsersController < ApplicationController
 	    end
   	end
 
+  	def index
+  		
+  	end
+  	
   	def edit
   	end
 
@@ -38,13 +42,15 @@ class UsersController < ApplicationController
 
   	private
 
-    def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
+	    def signed_in_user
+	    	unless signed_in?
+	    		store_location
+	      		redirect_to signin_url, notice: "Please sign in." unless signed_in?
+	    end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
-    end
+	    def correct_user
+	      @user = User.find(params[:id])
+	      redirect_to(root_path) unless current_user?(@user)
+	    end
+	end
 end
-
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -48,6 +48,22 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end
+      end
+
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -59,8 +75,12 @@ describe "Authentication" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+        	before { visit user_path}
+        	it {should have_selector('title', text:'Sign in')}
+        end
       end
     end
   end
-
 end
